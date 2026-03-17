c3f347aa061de5bddae87b96526049f4858964c9
Sky Blue <dawadiaakash@gmail.com>
2026-03-17 22:20:34 +0545

    fix: customUsers urls

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
-from .views import UserRegistrationView, UserViewSet, MeView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, UserRegistrationView, MeView
 
-user_list = UserViewSet.as_view({"get": "list"})
-soft_deleted = UserViewSet.as_view({"get": "list_soft_deleted"})
+router = DefaultRouter()
+router.register(r"", UserViewSet, basename="user")
 
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="user-register"),
     path("me/", MeView.as_view(), name="me"),
-    path("users/", user_list, name="user-list"),
-    path("users/soft-deleted/", soft_deleted, name="user-soft-deleted"),
+    path("", include(router.urls)),
 ]
