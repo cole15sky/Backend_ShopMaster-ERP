f17cf8b4482849db0d02e6cfd97134853d2ae481
Sky Blue <dawadiaakash@gmail.com>
2026-03-17 22:02:05 +0545

    feat: update jwt urls and customUser urls to core.urls

--- core/urls.py
+++ core/urls.py
@@ -3,16 +3,23 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
 
     path("admin/", admin.site.urls),
+    
+    # JWT Authentication endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path("api/products/", include("products.urls")),
     path("api/inventory/", include("inventory.urls")),
     path("api/sales/", include("sales.urls")),
     path("api/customers/", include("customers.urls")),
     path("api/qr/", include("qr.urls")),
+    path("api/users/", include("users.urls")),
     
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
