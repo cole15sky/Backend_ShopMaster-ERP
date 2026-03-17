fbc134e3bda40b035f9b8500bb1a7f0da6e03e6e
Sky Blue <dawadiaakash@gmail.com>
2026-03-17 21:30:14 +0545

    feat: add userCreation within admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,72 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+
+    ordering = ["id"]
+
+    list_display = (
+        "id",
+        "email",
+        "role",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
+
+    search_fields = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+
+        ("Personal info", {
+            "fields": (
+                "full_name",
+                "phone",
+                "profile_pic",
+                "position",
+                "role",
+            )
+        }),
+
+        ("Permissions", {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )
+        }),
+
+        ("Important dates", {
+            "fields": ("last_login", "date_joined")
+        }),
+    )
+
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "role",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
