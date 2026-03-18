3e5d8d9f694535f70655da39b4470c886db82388
Sky Blue <dawadiaakash@gmail.com>
2026-03-18 20:35:14 +0545

    fix: product urls

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import ProductListView
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import BrandViewSet, CategoryViewSet, ProductViewSet, ProductVariantViewSet
+
+router = DefaultRouter()
+router.register("brands", BrandViewSet, basename="brand")
+router.register("categories", CategoryViewSet, basename="category")
+router.register("products", ProductViewSet, basename="product")
+router.register("variants", ProductVariantViewSet, basename="variant")
 
 urlpatterns = [
-    path("", ProductListView.as_view()),
+    path("", include(router.urls)),
 ]
