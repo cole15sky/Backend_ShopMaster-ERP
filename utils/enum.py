28c7385a630c03fc592686e2c7bd6c2d53b074dc
Sky Blue <dawadiaakash@gmail.com>
2026-03-18 14:48:44 +0545

    feat: create enum class for product app

--- utils/enum.py
+++ utils/enum.py
@@ -6,3 +6,20 @@ class UserRole(models.TextChoices):
     ADMIN = "ADMIN", "Admin"
     STAFF = "STAFF", "Staff"
     CUSTOMER = "CUSTOMER", "Customer"
+    
+class GenderType(models.TextChoices):
+    MALE = "Male", "Male"
+    FEMALE = "Female", "Female"
+    UNISEX = "Unisex", "Unisex"
+
+class SizeType(models.TextChoices):
+    XS = "XS", "Extra Small"
+    S = "S", "Small"
+    M = "M", "Medium"
+    L = "L", "Large"
+    XL = "XL", "Extra Large"
+    XXL = "XXL", "Double Extra Large"
+
+class ProductStatus(models.TextChoices):
+    ACTIVE = "Active", "Active"
+    INACTIVE = "Inactive", "Inactive"
