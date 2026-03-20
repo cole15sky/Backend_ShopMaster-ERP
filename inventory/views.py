c5d2d95b7b11b12fe81ada180d88ce5168b6cb8f
Sky Blue <dawadiaakash@gmail.com>
2026-03-20 20:22:35 +0545

    feat: create views for the inventory

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models.inventory import Inventory
+from .serializers import InventorySerializer
 
-# Create your views here.
+
+class InventoryViewSet(viewsets.ModelViewSet):
+
+    queryset = Inventory.objects.all()
+    serializer_class = InventorySerializer
