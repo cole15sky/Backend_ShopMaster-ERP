375cf28ecbe3365270ac7a764c6d0e2863f80df0
Aakash Dawadi <dawadiaakash@gmail.com>
2026-06-02 08:16:48 +0545

    fix: User role default on Customer

--- users/models.py
+++ users/models.py
@@ -14,7 +14,7 @@ class User(AbstractUser):
     role = models.CharField(
         max_length=20,
         choices=UserRole.choices,
-        default=UserRole.ADMIN,
+        default=UserRole.CUSTOMER,
     )
 
     full_name = models.CharField(
