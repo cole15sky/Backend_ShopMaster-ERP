1480ad09d67661c6339e45626078407b7d4555a7
Aakash Dawadi <dawadiaakash@gmail.com>
2026-06-03 07:26:01 +0545

    feat: add invenotorySerializers

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
 from products.models.product_variant import ProductVariant
 from utils.enum import UnitType
+from .models.inventory import Inventory
+
+
+class InventorySerializer(serializers.ModelSerializer):
+    variant_name= serializers.CharField(source="variant.sku", read_only=True)
+    is_low_stock = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Inventory
+        fields = ["id", "variant", "variant_name", "quantity", "is_low_stock","low_stock_alert", "updated_at"]
 
 
 class StockInSerializer(serializers.Serializer):
