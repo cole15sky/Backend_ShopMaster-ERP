53f8bf93d879ebe880beed59cf1fe53ec1fcc50d
Aakash Dawadi <dawadiaakash@gmail.com>
2026-06-05 17:37:31 +0545

    fix: auto data/ creation

--- core/settings.py
+++ core/settings.py
@@ -4,6 +4,10 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / 'data'
+
+# Automatically create the 'data' directory if it doesn't exist
+DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 
 # Quick-start development settings - unsuitable for production
